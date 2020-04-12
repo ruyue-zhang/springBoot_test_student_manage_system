19a9fbcb20378caa21cca1194423ceee4255104f
lpdft <772346730@qq.com>
2020-04-12 22:26:30 +0800

    [ruyue] feat: finish add student

---
+++ src/main/java/com/example/demo/StudentManagement.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StudentManagement {
+    Map<String,Student> studentMap = new LinkedHashMap<>();
+
+    @PostMapping("/add")
+    public String post(@RequestBody Student student) {
+        if(studentMap.containsKey(student.getName())) {
+            return new String("姓名重复");
+        } else {
+            studentMap.put(student.getName(),student);
+            return new String("添加成功");
+        }
+    }
+
+    
+}
